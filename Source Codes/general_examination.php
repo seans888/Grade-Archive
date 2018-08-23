aa2d10be9ba0e914fa7c95df6da6aa68181dbefe
patrickcedie <39973179+patrickcedie@users.noreply.github.com>
2018-08-24 05:20:11 +0800

    Source Codes/general_examination.php

---
+++ Source Codes/general_examination.php
@@ -0,0 +1,13 @@
+<?php
+require_once  '../database_connection/database_connection.php';
+require_once '../top_header_and_footer/top_header_section.php';
+
+?>
+
+<?php require_once '../navigation_section/navigation_classadviser_file.php';?>
+
+<?php  require_once '../top_header_and_footer/footer_section.php'; ?>
+
+<!-- If you are done with everything  -->
+
+<?php require '../top_header_and_footer/final_closing_tag.php' ?>
